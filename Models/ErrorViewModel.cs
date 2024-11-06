a2ef1178c9bfecabd11076dab13623ddcee2196f
Evandro Maciel <juniorjsn2010@gmail.com>
2024-11-06 15:28:59 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcoFarm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
